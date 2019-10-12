29f0074fce190da6ea90fa99daf57c81bc1c2289
Tavis Weeden <tavisweeden@gmail.com>
2019-10-12 01:19:25 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,11 +15,11 @@
 
 
 def speak_to_grandma(phrase)
-  if phrase == phrase.upcase
-   "NO, NOT SINCE 1938!"
- elsif phrase == "I LOVE YOU GRANDMA!" 
+  if phrase == "I LOVE YOU GRANDMA!" 
    "I LOVE YOU TOO PUMPKIN!"
-else
+  elsif phrase == phrase.upcase
+   "NO, NOT SINCE 1938!"
+  else
    "HUH?! SPEAK UP, SONNY!"
   
   end
